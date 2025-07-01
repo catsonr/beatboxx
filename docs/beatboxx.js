c8466692f93c6dd9064eaf6cc310bf2eab1cb632
carsonritchey <ritcheycarson@gmail.com>
2025-06-30 19:49:10 -0500

    test

--- docs/beatboxx.js
+++ docs/beatboxx.js
@@ -45,7 +45,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /var/folders/v8/kv5tdb4s5d57h2swgmltjd580000gn/T/tmp84jn819m.js
+// include: /var/folders/v8/kv5tdb4s5d57h2swgmltjd580000gn/T/tmpj384pfpn.js
 Module["expectedDataFileDownloads"] ??= 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -9780,23 +9780,23 @@ Module["expectedDataFileDownloads"]++;
   });
 })();
 
-// end include: /var/folders/v8/kv5tdb4s5d57h2swgmltjd580000gn/T/tmp84jn819m.js
-// include: /var/folders/v8/kv5tdb4s5d57h2swgmltjd580000gn/T/tmpsg0ujk4t.js
+// end include: /var/folders/v8/kv5tdb4s5d57h2swgmltjd580000gn/T/tmpj384pfpn.js
+// include: /var/folders/v8/kv5tdb4s5d57h2swgmltjd580000gn/T/tmphjkk8gd6.js
 // All the pre-js content up to here must remain later on, we need to run
 // it.
 if ((typeof ENVIRONMENT_IS_WASM_WORKER != "undefined" && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != "undefined" && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != "undefined" && ENVIRONMENT_IS_AUDIO_WORKLET)) Module["preRun"] = [];
 
 var necessaryPreJSTasks = Module["preRun"].slice();
 
-// end include: /var/folders/v8/kv5tdb4s5d57h2swgmltjd580000gn/T/tmpsg0ujk4t.js
-// include: /var/folders/v8/kv5tdb4s5d57h2swgmltjd580000gn/T/tmpl71qg7uz.js
+// end include: /var/folders/v8/kv5tdb4s5d57h2swgmltjd580000gn/T/tmphjkk8gd6.js
+// include: /var/folders/v8/kv5tdb4s5d57h2swgmltjd580000gn/T/tmp6__ua1yn.js
 if (!Module["preRun"]) throw "Module.preRun should exist because file support used it; did a pre-js delete it?";
 
 necessaryPreJSTasks.forEach(task => {
   if (Module["preRun"].indexOf(task) < 0) throw "All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?";
 });
 
-// end include: /var/folders/v8/kv5tdb4s5d57h2swgmltjd580000gn/T/tmpl71qg7uz.js
+// end include: /var/folders/v8/kv5tdb4s5d57h2swgmltjd580000gn/T/tmp6__ua1yn.js
 var arguments_ = [];
 
 var thisProgram = "./this.program";
